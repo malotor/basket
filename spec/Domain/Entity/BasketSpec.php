061fce76077677bcf25bf44575a4d0f6f8159ead
Manel <malotor@gmail.com>
2015-05-12 22:37:46 +0200

    RED it_shoul_retrieve_an_item

--- spec/Domain/Entity/BasketSpec.php
+++ spec/Domain/Entity/BasketSpec.php
@@ -6,6 +6,7 @@ use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 use malotor\shoppingcart\Domain\Entity\Item;
+use malotor\shoppingcart\Domain\ValueObject\Identifier;
 
 class BasketSpec extends ObjectBehavior
 {
@@ -22,5 +23,12 @@ class BasketSpec extends ObjectBehavior
     function it_should_have_1_item_when_a_new_item_is_added(Item $item) {
     	$this->addItem($item);
 		$this->countItems()->shouldReturn(1);
+	}
+
+	function it_shoul_retrieve_an_item(Item $item) {
+		$itemId = "1";
+		$item->getId()->willReturn($itemId);
+		$this->addItem($item);
+		$this->getItem($itemId)->shouldReturn($item);
 	}
 }
