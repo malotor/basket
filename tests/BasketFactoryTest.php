36c75f0f7736df896dd537f1949528be1828d67f
Manel <malotor@gmail.com>
2015-05-14 16:31:45 +0200

    RED testShoudlReturnABasketWithProducts

--- tests/BasketFactoryTest.php
+++ tests/BasketFactoryTest.php
@@ -7,9 +7,22 @@ use malotor\shoppingcart\Domain\Entity\Product;
 class BasketFactoryTest extends PHPUnit_Framework_TestCase {
 
     public function testShouldReturnABasketObject() {
-
+        
         $basket = BasketFactory::create();
 
         $this->assertTrue($basket instanceof Basket);
     }
+
+    public function testShoudlReturnABasketWithProducts()
+    {
+        $products[0] = new Product(1, 10);
+        $products[1] = new Product(2, 29.2);
+        
+        $basket = BasketFactory::create($products);
+
+        $product = $basket->getItem(1);
+    
+        $this->assertEquals($products[0], $product);
+    }
+
 }
