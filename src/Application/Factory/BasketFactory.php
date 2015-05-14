6d199e648d15940b3450b9acd0f296719ebe1989
Manel <malotor@gmail.com>
2015-05-14 16:31:55 +0200

    GREEN testShoudlReturnABasketWithProducts

--- src/Application/Factory/BasketFactory.php
+++ src/Application/Factory/BasketFactory.php
@@ -6,7 +6,11 @@ use malotor\shoppingcart\Domain\Entity\Basket;
 
 class BasketFactory
 {
-    static public function create() {
-        return new Basket();
+    static public function create($products = array()) {
+        $basket = new Basket();
+        foreach ($products as $product) {
+           $basket->addItem($product);
+        }
+        return $basket;
     }
 }
