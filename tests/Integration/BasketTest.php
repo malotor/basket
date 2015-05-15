72802f0c6a3a041dded2f3a75c1ac7b0d0cd8677
Manel <malotor@gmail.com>
2015-05-15 18:39:55 +0200

    Basket integration tests created

---
+++ tests/Integration/BasketTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use malotor\shoppingcart\Domain\Entity\Basket;
+use malotor\shoppingcart\Domain\Entity\Product;
+
+
+class BasketTest extends PHPUnit_Framework_TestCase {
+
+    protected $basket;
+
+    public function setUp() {
+        $this->products[0] = new Product(1,10);
+        $this->products[1] = new Product(2,5.5);
+
+        $this->basket = new Basket();
+    }
+
+    public function testAddAProduct() {
+        $this->assertEquals(0, $this->basket->countItems());
+        $this->basket->addItem($this->products[0]);
+        $this->assertEquals(1, $this->basket->countItems());
+    }
+    public function testRemoveAProduct() {
+        $this->basket->addItem($this->products[0]);
+        $this->assertEquals(1, $this->basket->countItems());
+        $this->basket->addItem($this->products[1]);
+        $this->assertEquals(2, $this->basket->countItems());
+    }
+
+    public function testIncreaseProductQuantity() {
+        $this->basket->addItem($this->products[0]);
+        $this->assertEquals(1, $this->basket->countItems());
+        $this->basket->addItem($this->products[0]);
+        $this->assertEquals(1, $this->basket->countItems());
+    }
+
+    public function testCartTotalAmount() {
+        $this->basket->addItem($this->products[0]);
+        $this->basket->addItem($this->products[0]);
+        $this->basket->addItem($this->products[1]);
+        $this->assertEquals(25.5, $this->basket->totalAmount());
+    }
+}
